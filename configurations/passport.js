8f6f9f5b0c11394a8f6694ff6992544238735f94
Karim Hesham <kimoantiqe@hotmail.com>
2018-09-26 16:16:17 -0700

    fixed auth service

--- configurations/passport.js
+++ configurations/passport.js
@@ -1,15 +1,17 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('../models/user');
+const JwtStrategy    = require('passport-jwt').Strategy;
+const ExtractJwt     = require('passport-jwt').ExtractJwt;
+const User           = require('../models/user');
+const jwtConfig      = require('../configurations/jwt.js');
+const {to}        = require('../services/util');
 
 module.exports = function(passport){
     var opts = {};
     opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-    opts.secretOrKey = 'change_me';
+    opts.secretOrKey = jwtConfig.encryption;
 
     passport.use(new JwtStrategy(opts, async function(jwt_payload, done){
         let err, user;
-        [err, user] = await to(User.findById(jwt_payload.user_id).populate('books','title'));
+        [err, user] = await to(User.findById(jwt_payload.user_id));
         if(err) return done(err, false);
         if(user) {
             return done(null, user);
