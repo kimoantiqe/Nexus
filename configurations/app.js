9c7299675767317ad85936ed0369e7347902b68b
Karim Hesham <kimoantiqe@hotmail.com>
2018-09-26 01:53:28 -0700

    Added new port num

--- configurations/app.js
+++ configurations/app.js
@@ -1,6 +1,6 @@
 let appConfig = {}
 
 appConfig.app          = process.env.APP   || 'development';
-appConfig.port         = process.env.PORT  || '3000';
+appConfig.port         = process.env.PORT  ||  '1337';
 
 module.exports = appConfig;
