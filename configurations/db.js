cec39bae27248fd7445f5964568771531f228a9c
Karim Hesham <kimoantiqe@hotmail.com>
2018-09-26 01:46:41 -0700

    Changed db url

--- configurations/db.js
+++ configurations/db.js
@@ -1,11 +1,8 @@
 let dbConfig = {}
 
 dbConfig.dialect   = process.env.DB_DIALECT    || 'mongo';
-dbConfig.host      = process.env.DB_HOST       || 'localhost';
-dbConfig.port      = process.env.DB_PORT       || '27017';
-dbConfig.url       = process.env.DB_URL        || 'mongodb://127.0.0.1:27017';
+dbConfig.url       = process.env.DB_URL        || 'mongodb://networkinc:yQFp5qdTwCC7kXwd52gve0YSrEK93ISifxWjkQMng1JjSLAUxfDtfkDDWIBLqD1IV82I369msbniX7shoQ8SkQ==@networkinc.documents.azure.com:10255/?ssl=true';
 dbConfig.name      = process.env.DB_NAME       || 'NetworkINC';
-dbConfig.user      = process.env.DB_USER       || 'root';
-dbConfig.password  = process.env.DB_PASSWORD   || 'db-password';
+
 
 module.exports = dbConfig;
