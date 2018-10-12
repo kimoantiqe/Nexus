2db745fb8cd4883ea481a3a9373544a7262d8b3c
Karim Hesham <kimoantiqe@hotmail.com>
2018-10-12 01:36:42 -0700

    Changed db url

--- configurations/db.js
+++ configurations/db.js
@@ -1,7 +1,7 @@
 let dbConfig = {}
 
 dbConfig.dialect   = process.env.DB_DIALECT    || 'mongo';
-dbConfig.url       = process.env.DB_URL        || 'mongodb://networkinc:yQFp5qdTwCC7kXwd52gve0YSrEK93ISifxWjkQMng1JjSLAUxfDtfkDDWIBLqD1IV82I369msbniX7shoQ8SkQ==@networkinc.documents.azure.com:10255/?ssl=true';
+dbConfig.url       = process.env.DB_URL        || 'mongodb://nexus-restapi:bwwJQw27yVAyXVcXfC0jf3LynSCn4E9jvY3vvG86DU2sXAjMpGzUsvoBlV65uVi2mXR8Fv2aYaESRdfv3DPp2Q%3D%3D@nexus-restapi.documents.azure.com:10255/?ssl=true';
 dbConfig.name      = process.env.DB_NAME       || 'NetworkINC';
 
 
