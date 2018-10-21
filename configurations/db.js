2bc7131669d4d3633e21e7979469b9b8a0078f75
Karim Hesham <kimoantiqe@hotmail.com>
2018-10-21 07:13:55 -0700

    Added docker compose

--- configurations/db.js
+++ configurations/db.js
@@ -1,7 +1,7 @@
 let dbConfig = {}
 
 dbConfig.dialect   = process.env.DB_DIALECT    || 'mongo';
-dbConfig.url       = process.env.DB_URL        || 'mongodb://nexus-restapi:bwwJQw27yVAyXVcXfC0jf3LynSCn4E9jvY3vvG86DU2sXAjMpGzUsvoBlV65uVi2mXR8Fv2aYaESRdfv3DPp2Q%3D%3D@nexus-restapi.documents.azure.com:10255/?ssl=true';
+dbConfig.url       = process.env.DB_URL        || 'mongodb://mongo:27017';//'mongodb://nexus-worker:nexus321@/13.77.183.17:8080/Nexus';//'mongodb://nexus-restapi:bwwJQw27yVAyXVcXfC0jf3LynSCn4E9jvY3vvG86DU2sXAjMpGzUsvoBlV65uVi2mXR8Fv2aYaESRdfv3DPp2Q%3D%3D@nexus-restapi.documents.azure.com:10255/?ssl=true';
 dbConfig.name      = process.env.DB_NAME       || 'NetworkINC';
 
 
