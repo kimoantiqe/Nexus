14c1d2977557b15a3f353d5078f216ccfc186363
Yousef Aboelmagd <yousefaboelmagd@server.ca>
2018-11-18 14:06:57 -0800

    Native-base components + register clean up

--- screens/Register.js
+++ screens/Register.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
-import { LinearGradient } from 'expo';
-import {
-  ActivityIndicator,
-  AsyncStorage,
-  StatusBar,
-  StyleSheet,
-  View,
-  Image,
-  Text,
-  Dimensions,
-   KeyboardAvoidingView, Alert, TextInput,
-} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
+import Background from '../components/Background';
+import { Container, Content, Form, Input, Button, Item, Text } from 'native-base';
+
 import MainTabNavigator from '../navigation/MainTabNavigator';
 import AppNavigator from '../navigation/AppNavigator';
-import Background from '../components/Background';
-import { Container, Content, Form, Input, Button, Item } from 'native-base';
+
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
+
 const API = require("../API_calls/APIs");
+
+
 export default class Register extends React.Component {
   static navigationOptions = {
     header: null,
@@ -40,20 +34,20 @@ export default class Register extends React.Component {
     <Content>
         <Background logo= {true}/>
         <Form style={{paddingHorizontal: width*0.05, justifyContent: 'space-evenly'}}>
-            <Text style={{color: '#f2f2f2', fontSize: 20, fontWeight: '300', padding: width*0.02}}>Email Address</Text>
+            <Text style={styles.Text}>Email Address</Text>
             <Item rounded style={{ paddingHorizontal: width*0.02}}>
-                <Input  style={{color: '#f2f2f2', fontSize: 22, fontWeight: '400'}} onChangeText = {(text)=>this.handleInput("Username", text)}/>
+                <Input  style={styles.InputText} onChangeText = {(text)=>this.handleInput("Username", text)}/>
             </Item>
-            <Text style={{color: '#f2f2f2', fontSize: 20, fontWeight: '300', padding: width*0.02}}>Password</Text>
+            <Text style={styles.Text}>Password</Text>
             <Item rounded style={{ paddingHorizontal: width*0.02}}>
-                <Input style={{color: '#f2f2f2', fontSize: 22, fontWeight: '400'}} secureTextEntry onChangeText = {(text)=>this.handleInput("Password", text)}/>
+                <Input style={styles.InputText} secureTextEntry onChangeText = {(text)=>this.handleInput("Password", text)}/>
             </Item>
-            <Text style={{color: '#f2f2f2', fontSize: 20, fontWeight: '300', padding: width*0.02}}>Confirm Password</Text>
+            <Text style={styles.Text}>Confirm Password</Text>
             <Item rounded style={{ paddingHorizontal: width*0.02}}>
-                <Input style={{color: '#f2f2f2', fontSize: 22, fontWeight: '400'}} secureTextEntry onChangeText = {(text)=>this.handleInput("Repassword", text)}/>
+                <Input style={styles.InputText} secureTextEntry onChangeText = {(text)=>this.handleInput("Repassword", text)}/>
             </Item>
-            <Content scrollEnabled={false} contentContainerStyle={{flexDirection: 'row', justifyContent: 'center', padding: width*0.05}}>
-            <Button rounded style={{backgroundColor: '#16131d', borderWidth: width*0.003, borderColor: 'white', paddingHorizontal: width*0.05}} onPress = {()=>API.Register(this.state.inputs, this.props)}>
+            <Content scrollEnabled={false} contentContainerStyle={styles.ButtonContainer}>
+            <Button rounded style={styles.Button} onPress = {()=>API.Register(this.state.inputs, this.props)}>
                 <Text style={{color: '#f2f2f2', fontSize: 20, fontWeight: '300'}}>Register</Text>
             </Button>
             </Content>
@@ -67,80 +61,28 @@ export default class Register extends React.Component {
 };
 
 const styles = StyleSheet.create({
-    containter: {
-        padding: 30,
-    },
-    input: {
-        height: 50,
-        backgroundColor: 'rgba(255, 255, 255, 0.2)',
-        marginBottom: 17,
-        color: '#FFF',
-        paddingHorizontal: 10,
-        fontSize: 20
-    }
-});
-
-const styles1 = StyleSheet.create({
-    contaier: {
-        flex: 1,
-        backgroundColor: 'transparent',
-        flexDirection:'column',
-    },
-    container: {
-        flex: 1,
-        backgroundColor: 'rgba(255, 0, 0, 1)',
-        position:'absolute',
-
-    },
-    logoContainer: {
-        paddingTop:50,
-        paddingBottom:30,
-        alignItems: 'center',
-        flex: 0.3,
-        justifyContent: 'center',
-    },
-    logoImage: {
-        width: width*0.82,
-        height: height*0.25,
-        resizeMode: 'contain'
-
-    },
-    formContainter: {
-      flex:0.5,
-      height:50,
-        justifyContent: 'center',
-        alignItems: 'center',
-
-    },
-    cloudcon: {
-      position: 'absolute',
-      width:width*.2,
-      height:height*.1,
-    },
-    cloud: {
-      marginTop:height*.05,
-      opacity:0.04,
-      width:width*.4,
-      height:height*1.2,
-      resizeMode:'contain',
-
-    },
-    cloud2: {
-      marginTop:height*.17,
-      marginRight:width*.4,
-      opacity:0.04,
-      width:width*0.7,
-      height:height*.07,
-      resizeMode:'contain',
-
-    },
-    cloud3: {
-      marginTop:height*.46,
-      marginLeft:width*.3,
-      opacity:0.03,
-      width:width*1.2,
-      height:height*.13,
-      resizeMode:'contain',
-
-    }
+    Button:         {
+                        backgroundColor: 'transparent', 
+                        borderWidth: width*0.003, 
+                        borderColor: 'white', 
+                        width: width*0.35,
+                        flexDirection: 'row', 
+                        justifyContent: 'center'
+                    },
+    ButtonContainer:{
+                        flexDirection: 'row', 
+                        justifyContent: 'center', 
+                        padding: width*0.05
+                    },
+    Text:           {
+                        color: '#f2f2f2', 
+                        fontSize: 20, 
+                        fontWeight: '300', 
+                        padding: width*0.02
+                    },
+    InputText:      {
+                        color: '#f2f2f2', 
+                        fontSize: 22, 
+                        fontWeight: '400'
+                    }
 });
--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,21 +1,11 @@
 import React, { Component } from "react";
-import { LinearGradient } from "expo";
-import {
-  ActivityIndicator,
-  AsyncStorage,
-  StatusBar,
-  StyleSheet,
-  View,
-  Image,
-  Dimensions,
-  KeyboardAvoidingView,
-  Alert,
-  TextInput,
-  TouchableOpacity
-} from "react-native";
-import {Text,Button} from "native-base";
+import {StyleSheet, Dimensions} from "react-native";
+import {Text, Button, Container, Content, Form} from "native-base";
+
 import MainTabNavigator from "../navigation/MainTabNavigator";
 import AppNavigator from "../navigation/AppNavigator";
+
+import Background from '../components/Background';
 
 //import actions for login and sendbird
 import { connect } from "react-redux";
@@ -36,63 +26,25 @@ export default class Splash extends React.Component {
 
   render() {
     return (
-      <View style={styles1.container}>
-        <View style={{ flex: 0.5, opacity: 1 }}>
-          <LinearGradient
-            colors={["#16131d", "#734b6d"]}
-            locations={[0.0, height * 0.0095]}
-            style={{
-              position: "absolute",
-              left: 0,
-              right: 0,
-              top: 0,
-              height: height
-            }}
-          />
-
-
-          <View style={styles1.cloudcon}>
-            <Image
-              style={styles1.cloud}
-              source={require("../images/cloud3.png")}
-            />
-          </View>
-
-          <View style={styles1.cloudcon}>
-            <Image
-              style={styles1.cloud2}
-              source={require("../images/cloud3.png")}
-            />
-          </View>
-          <View style={styles1.cloudcon}>
-            <Image
-              style={styles1.cloud3}
-              source={require("../images/cloud3.png")}
-            />
-          </View>
-
-          <View style={styles1.content}>
-            <View style={styles1.logoContainer}>
-              <Image
-                style={styles1.logoImage}
-                source={require("../images/logoGold.png")}
-              />
-            </View>
-
-            <TouchableOpacity style={styles1.buttonContainer}>
-              <Button onPress={this.Login}  style={styles1.button}>
-                <Text style={ {fontWeight: 'bold'}}>LOGIN</Text>
-              </Button>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles1.buttonContainer}>
-              <Button onPress={this.Register}  style={styles1.button}>
-                <Text style={ {fontWeight: 'bold'}} >SIGN UP</Text>
-              </Button>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
+      <Container>
+        <Content style={styles.container}>
+        <Background logo={true} />
+        <Content scrollEnabled={false} contentContainerStyle={styles.ButtonGroup}>
+        <Form>
+        <Form style={styles.buttonContainer}>
+            <Button rounded style={styles.Button} onPress={this.Login}>
+                <Text style={{ fontWeight: 'bold'}}>LOGIN</Text>
+            </Button>
+            </Form>
+            <Form style={styles.buttonContainer}>
+            <Button rounded style={styles.Button} onPress={this.Register}>
+                <Text style={{ fontWeight: 'bold'}}>SIGN UP</Text>
+            </Button>
+            </Form>
+            </Form>
+          </Content>
+          </Content>
+          </Container>
     );
   }
 
@@ -103,77 +55,29 @@ export default class Splash extends React.Component {
   Login = async () => {
     this.props.navigation.navigate("Login");
   };
-}
+};
 
-const styles1 = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "transparent",
-    flexDirection: "column"
-  },
-  content:{
-    flex:0.7
-  },
-  logoContainer: {
-    paddingTop: height/3,
-    paddingBottom: height/4.5,
-    alignItems: "center",
-    flex: 0.3,
-    justifyContent: "center"
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    width: "100%",
-    height: "30%",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  button: {
-    width: 200,
-    backgroundColor: "transparent",
-    borderColor: "grey",
-    borderWidth: 1,
-    borderRadius: 100,
-    marginTop: 20,
-    justifyContent: 'center'
-  },
-  logoImage: {
-    width: width * 0.9,
-    height: height * 0.31,
-    resizeMode: "contain"
-  },
-  formContainter: {
-    flex: 0.5,
-    height: 50,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  cloudcon: {
-    position: "absolute",
-    width: width * 0.2,
-    height: height * 0.1
-  },
-  cloud: {
-    marginTop: height * 0.05,
-    opacity: 0.04,
-    width: width * 0.4,
-    height: height * 1.2,
-    resizeMode: "contain"
-  },
-  cloud2: {
-    marginTop: height * 0.17,
-    marginRight: width * 0.4,
-    opacity: 0.04,
-    width: width * 0.7,
-    height: height * 0.07,
-    resizeMode: "contain"
-  },
-  cloud3: {
-    marginTop: height * 0.46,
-    marginLeft: width * 0.3,
-    opacity: 0.03,
-    width: width * 1.2,
-    height: height * 0.13,
-    resizeMode: "contain"
-  }
-});
+const styles = StyleSheet.create({
+  ButtonGroup:   { 
+                      flex: 1, 
+                      flexDirection: 'column',
+                      alignItems: 'center',
+                      paddingTop: height*0.1
+                  },
+  buttonContainer:{
+                      padding: width*0.02
+                  },
+  Button:         {
+                      backgroundColor: 'transparent', 
+                      borderWidth: width*0.002, 
+                      borderColor: 'grey', 
+                      width: width*0.45, 
+                      flexDirection: 'row', 
+                      justifyContent: 'center'
+                  },
+  container:      {
+                    flex: 1,
+                    backgroundColor: "transparent",
+                    flexDirection: "column"
+                  },
+                              });
