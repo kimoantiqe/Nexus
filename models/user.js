b6841d203ded4ab3f100aaf83b417109764c6e8d
Karim Hesham <kimoantiqe@hotmail.com>
2018-11-25 05:10:15 -0800

    Setup facebookid index to be sparse

--- models/user.js
+++ models/user.js
@@ -10,7 +10,8 @@ const jwtConfig = require('../configurations/jwt');
 let UserSchema = mongoose.Schema({
   facebookId:{
     type:String,
-    index: true
+    index: true,
+    sparse: true
   },
   image:{
     type:mongoose.Schema.Types.ObjectId,
